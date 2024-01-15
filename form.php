1a04f80520098acbf8db09202cd8256407b612ff
cyclone <tulyatimo@gmail.com>
2024-01-15 21:27:34 +0300

    added login form

---
+++ form.php
@@ -0,0 +1,25 @@
+<?php 
+
+?>
+
+<html>
+    <head>
+        <title>Form</title>
+    </head>
+    <body>
+        
+        <form action="">
+            <label for="">Title:</label>
+            <input type="text" name="title">
+
+            <label for="">emial:</label>
+            <input type="email" name="title">
+
+            <label for="">password:</label>
+            <input type="password" name="title">
+
+            <input type="submit" value="submit" name="submit">
+        </form>
+
+    </body>
+</html>
